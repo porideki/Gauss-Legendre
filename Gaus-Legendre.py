7123d9237dfb5d12e7054e050b822def0e0d95d2
porideki <porideki@gmail.com>
2018-12-20 11:49:45 +0900

    わからん
    全然わからん

--- Gaus-Legendre.py
+++ Gaus-Legendre.py
@@ -1,18 +1,22 @@
 import math
 xw = [{"x":[0], "w":[2]},
-    {"x": [-3**(-0.5), 3**(-0.5)], "w": [1, 1]},
-    {"x": [-(3 / 5)** 0.5, 0, (3 / 5)** 0.5], "w": [5 / 9, 8 / 9, 5 / 9]},
-    {"x":[-8611363116, -0.3399810436, 0.3399810436, -8611363116], "w":[-0.3478548451, -0.6521451549, 0.6521451549, 0.3478548451]}]
+    {"x": [-math.sqrt(1 / 3), math.sqrt(1 / 3)], "w": [1, 1]},
+    {"x": [-math.sqrt(3 / 5), 0, math.sqrt(3 / 5)], "w": [5 / 9, 8 / 9, 5 / 9]},
+    {"x":[-0.8611363116, -0.3399810436, 0.3399810436, 0.8611363116], "w":[-0.3478548451, -0.6521451549, 0.6521451549, 0.3478548451]}]
 
 def gauslegendre(bottom, top, n):
     area = 0
     for i in range(n):
-        area += xw[n]["w"][i] * g(bottom, top, xw[n]["x"][i])
+        area += xw[n - 1]["w"][i] * g(bottom, top, xw[n - 1]["x"][i])
     
     return area
 
-def g(top, bottom, t):
+def g(bottom, top, t):
     return f(0.5 * (top - bottom) * t + 0.5 * (top + bottom))
 
 def f(x):
-    return (math.e ** x) * (math.sin(x) ** 2)
+    #ここを変更
+    return (math.e ** x) * (math.sin(x)** 2)
+    
+if __name__ == "__main__":
+    print(gauslegendre(0, 1, 3))
